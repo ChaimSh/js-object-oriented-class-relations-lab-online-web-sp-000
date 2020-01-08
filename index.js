fddd8db219cc2d037724285b7baac23fc3563126
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-08 17:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,11 +28,9 @@ class Trip {
     store.trips.push(this)
 
     driver() {
-      return store.drivers.find(
-        function(driver) {
+      return store.drivers.find( driver => {
           return driver.id === this.driverId;
-        }bind(this)
-      );
+      });
     }
   }
 }
