76a32576c525be0ae4ea37c57add5301f679775b
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-08 16:37:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+let store = {passengers: [], trips: [], drivers: []}
+
+
+class Driver{
+constructor(name){
+  this.name = name
+}
+}
+
+class Passenger{
+  constructor(name){
+  this.name = name
+}
+}
+
+class Trip {
+  // constructor
+}
